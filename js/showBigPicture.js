3de1e9fa2586ebde6c5b3d25af54dc891c10458c
Arcanumsbc <arcanumsbc@yandex.ru>
2023-03-30 18:34:03 +0300

    Открывается и закрывается (часть 2)

--- js/showBigPicture.js
+++ js/showBigPicture.js
@@ -1,5 +1,6 @@
 import { isEscapeKey } from './utils.js';
 
+const COMMENTS_LIMIT = 5;
 const body = document.querySelector('body');
 const bigPicture = body.querySelector('.big-picture');
 const bigPictureImage = bigPicture.querySelector('img');
@@ -8,12 +9,50 @@ const pictureLikesCount = bigPicture.querySelector('.likes-count');
 const pictureCommentsCount = bigPicture.querySelector('.comments-count');
 const pictureCaption = bigPicture.querySelector('.social__caption');
 const bigPictureCommentsCount = bigPicture.querySelector('.social__comment-count');
+const commentsList = bigPicture.querySelector('.social__comments');
+const commentTemplate = bigPicture.querySelector('.social__comment');
+const bigPictureCommentsLoader = document.querySelector('.comments-loader');
+
+const createCommentElement = ({id, avatar, name, message}) => {
+  const commentElement = commentTemplate.cloneNode(true);
+
+  commentElement.dataset.id = id;
+  commentElement.querySelector('.social__picture').src = avatar;
+  commentElement.querySelector('.social__picture').alt = name;
+  commentElement.querySelector('.social__text').textContent = message;
+
+  return commentElement;
+};
+
+const renderComments = (comments) => {
+  const commentsFragment = document.createDocumentFragment();
+
+  comments.forEach((comment) => {
+    commentsFragment.append(createCommentElement(comment));
+  });
+
+  commentsList.append(commentsFragment);
+};
+
+const loadComments = (comments) => {
+  const splicedComments = comments.splice(0, COMMENTS_LIMIT);
+
+  bigPictureCommentsLoader.classList.remove('hidden');
+  renderComments(splicedComments);
+
+  if (comments.length === 0) {
+    bigPictureCommentsLoader.classList.add('hidden');
+  }
+};
 
 const fillBigPicture = ({url, likes, description, comments}) => {
   bigPictureImage.src = url;
   pictureLikesCount.textContent = likes;
   pictureCommentsCount.textContent = comments.length;
   pictureCaption.textContent = description;
+
+  commentsList.innerHTML = ''; // Удаляет дефолтные комментарии из разметки
+  loadComments(comments);
 
   body.classList.add('modal-open');
   bigPicture.classList.remove('hidden');
@@ -31,6 +70,7 @@ const fillBigPicture = ({url, likes, description, comments}) => {
     }
   });
   document.removeEventListener('keydown', closeBigPicture);
+  bigPictureCommentsLoader.addEventListener('click', () => loadComments(comments));
 };
 
 export { fillBigPicture };
