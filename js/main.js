64588952a46ce48f39fb927901e28c075e77a678
Arcanumsbc <arcanumsbc@yandex.ru>
2023-04-02 20:04:23 +0300

    Помощь друга

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ import { generatePictures } from './data.js';
 import { renderPictures } from './render-pictures.js';
 import './form-upload-picture.js';
 import './validation-form.js';
+import './get-effect.js';
 
 renderPictures(generatePictures());
